aed1571ff9bd6f5961ae877e69af5c46197c5ff1
andri budianto <budianto.andri92@gmail.com>
2022-02-12 00:49:13 +0700

    'firebase-login'

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,52 +1,46 @@
-import React, { Component } from 'react';
-import {Container} from 'reactstrap'
+import React, { Component, useState } from 'react';
+import { Button, Container, Form, FormGroup, Input, Label } from 'reactstrap'
 import { withRouter } from '../libs/withRouter';
+import { auth } from '../service/firebase';
+import { signInWithEmailAndPassword } from 'firebase/auth';
 
-class Response extends String {json = () => JSON.parse(this)}
-
-  function mockFetch(url, {body}) {
-      const {email, password} = body
-      if(email !== 'admin@admin.com' || password !== 'password')
-      return Promise.reject("email atau password salah")
-
-    return Promise.resolve(
-        new Response(
-            JSON.stringify({accessToken: 'pura2token'})
-        )
-    )      
+const Login = (props) => {
+  const [inputValue, setInputValue] = useState({email: '', password:''})
+  const {email, password} = inputValue
+  const handleChange = (e) => {
+    const {name, value} = e.target
+    setInputValue({...inputValue, [name]: value})
   }
-
-  class Login extends Component {
-    state = {email: null, password: null}
-    set = name => e => this.setState({[name]: e.target.value})
-  
-    handleSubmit = e => {
-      console.log(this.props)
-      e.preventDefault()
-      const {email, password} = this.state
-      const {history} = this.props
-      mockFetch('http://api.example.com/v1/auth/login', {
-        body: {email, password}
-      })
-      .then(response => response.json())
-      .then(json => {
-        localStorage.setItem('token', json.accessToken)
-        this.props.history('/')
-      })
-      .catch(err => {alert(err)})
-    }
-  
-    render(){
-      return(
-        <Container>
-          <h1>Login</h1>
-          <form onSubmit={this.handleSubmit}>
-          <label>Email <input type="email" name="email" onChange={this.set('email')} /></label>
-          <label>Password <input type="password" name="password" onChange={this.set('password')} /></label>
-          <input type="submit" value="submit" />
-          </form>
-        </Container>
-      )
-    }
+  const handleSubmit = (e) => {
+    e.preventDefault()
+    signInWithEmailAndPassword(auth, email, password)
+    .then(userCredential => {
+      const isLogin = userCredential.user
+      if(isLogin){
+        setInputValue({email: '', password:''})
+        props.history('/')
+      }
+    })
+    .catch(err => {
+        alert(err.message)
+    })
   }
+  return (
+    <Container className='container-xl d-flex justify-content-center align-items-center flex-column vh-100'>
+      <h3 className='text-center'>Login</h3>
+      <Form inline onSubmit={handleSubmit} className='w-50 text-center'>
+        <FormGroup floating>
+          <Input id='email' name='email' type='email' placeholder='Email' value={email} onChange={handleChange} />
+          <Label for='email'>Email</Label>
+        </FormGroup>
+        <FormGroup floating>
+          <Input id='password' name='password' type='password' placeholder='Password' value={password} onChange={handleChange} />
+          <Label for='password'>Password</Label>
+        </FormGroup>
+        <Button color='primary' type='submit' className='w-100 btn-lg'>Login</Button>
+      </Form>
+    </Container>
+  )
+}
+
 export default withRouter(Login);
